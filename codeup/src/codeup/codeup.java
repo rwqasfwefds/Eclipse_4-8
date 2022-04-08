7c49df832777d519691f06f6acccb783a85aa4fd
rwqasfwefds <rjswo456@naver.com>
2022-04-08 17:44:59 +0900

    Initial commit

---
+++ codeup/src/codeup/codeup.java
@@ -0,0 +1,18 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int code = scanner.nextInt();
+		
+		char x = (char)code;
+		
+		System.out.println(x);
+	}
+
+}
